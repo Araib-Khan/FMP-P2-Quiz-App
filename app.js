ff8a8bfc7f7ef5f33315676d1d694ab1ff350502
Araib-Khan <maraibniazi@gmail.com>
2023-01-22 12:05:07 +0500

    Work Complete

--- app.js
+++ app.js
@@ -1,98 +1,156 @@
-const quizData = [
-    {
-        question: "Which language runs in a web browser?",
-        a: "Java",
-        b: "C",
-        c: "Python",
-        d: "javascript",
-        correct: "d",
-    },
-    {
-        question: "What does CSS stand for?",
-        a: "Central Style Sheets",
-        b: "Cascading Style Sheets",
-        c: "Cascading Simple Sheets",
-        d: "Cars SUVs Sailboats",
-        correct: "b",
-    },
-    {
-        question: "What does HTML stand for?",
-        a: "Hypertext Markup Language",
-        b: "Hypertext Markdown Language",
-        c: "Hyperloop Machine Language",
-        d: "Helicopters Terminals Motorboats Lamborginis",
-        correct: "a",
-    },
-    {
-        question: "What year was JavaScript launched?",
-        a: "1996",
-        b: "1995",
-        c: "1994",
-        d: "none of the above",
-        correct: "b",
-    },
-    {
-        question: "What is the current year?",
-        a: "2000",
-        b: "2001",
-        c: "2023",
-        d: "none of the above",
-        correct: "d",
-    },
-    {
-        question: "orange is fruit or color?",
-        a: "fruit",
-        b: "color",
-        c: "both a & b",
-        d: "none of the above",
-        correct: "c",
-    },
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+ import{getDatabase,ref,set} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyBfRcpEZNuZ3NaVa1RB58Icle17ftx8zjk",
+   authDomain: "quizz-9c6bc.firebaseapp.com",
+   databaseURL: "https://quizz-9c6bc-default-rtdb.firebaseio.com",
+   projectId: "quizz-9c6bc",
+   storageBucket: "quizz-9c6bc.appspot.com",
+   messagingSenderId: "164057725202",
+   appId: "1:164057725202:web:66cca92c4926e0771a1bcb"
+ };
+
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+const database = getDatabase();
+
+var a = document.getElementById("quiz");
+var b = document.getElementById("st");
+
+
+a.className += " dis";
+b.className += " ss";
+var questions = [
+  {
+    ques:"What is the capital of France?",
+    corect: "C) Paris",
+    opt: [
+      "A) Rome",
+      "B) London",
+      "C) Paris",
+      "D) Berlin",
+    ],
+  },
+  {
+    ques: "Who is the current President of the United States?",
+    corect: "A) Joe Biden",
+    opt: [
+      "A) Joe Biden",
+      "B) Donald Trump",
+      "C) Barack Obama",
+      "D) George Washington",
+    ],
+  },
+  {
+    ques: "What is the currency of Japan?",
+    corect: "A) Yen",
+    opt: ["A) Yen", "B) Dollar", "C) Euro", "D) Pound"],
+  },
+  {
+    ques: "Who is the author of the novel ?",
+    corect: "D) Harper Lee",
+    opt: [
+      "A) William Shakespeare",
+      "B) J.D. Salinger",
+      "C) John Steinbeck",
+      "D) Harper Lee",
+    ],
+  },
+  {
+    ques: "What is the highest mountain in the world?",
+    corect: "A) Mount Everest",
+    opt: [
+      "A) Mount Everest",
+      "B) K2",
+      "C) Makalu",
+      "D) Kangchenjunga",
+    ],
+  },
+  {
+    ques: "What is the capital of Australia?",
+    corect: "C) Canberra",
+    opt: ["A) Sydney", "B) Melbourne", "C) Canberra", "D) Perth"],
+  },
+  {
+    ques: "Who is the current President of Pakistan?",
+    corect: "A) Arif Alvi",
+    opt: ["A) Arif Alvi", "B) Asif Ali Zardari", "C) Pervez Musharraf", "D) Muhammad Zia-ul-Haq"],
+  },
+  {
+    ques: "What is the national animal of Pakistan?",
+    corect: "D) Markhor",
+    opt: [
+      "A) Tiger",
+      "B) Lion",
+      "C) Elephant",
+      "D) Markhor",
+    ],
+  },
+  {
+    ques: "What is the national sport of Pakistan?",
+    corect: "C) Field Hockey",
+    opt: ["A) Cricket", "B) Soccer", "C) Field Hockey", "D) Tennis"],
+  },
+  {
+    ques: "Who was the first Prime Minister of Pakistan?",
+    corect: "A) Liaquat Ali Khan",
+    opt: ["A) Liaquat Ali Khan", "B) Muhammad Ali Jinnah", "C) Zulfikar Ali Bhutto", "D) Nawaz Sharif"],
+  },
 ];
-const quiz= document.getElementById('quiz')
-const answerEls = document.querySelectorAll('.answer')
-const questionEl = document.getElementById('question')
-const a_text = document.getElementById('a_text')
-const b_text = document.getElementById('b_text')
-const c_text = document.getElementById('c_text')
-const d_text = document.getElementById('d_text')
-const submitBtn = document.getElementById('submit')
-let currentQuiz = 0
-let score = 0
-loadQuiz()
-function loadQuiz() {
-    deselectAnswers()
-    const currentQuizData = quizData[currentQuiz]
-    questionEl.innerText = currentQuizData.question
-    a_text.innerText = currentQuizData.a
-    b_text.innerText = currentQuizData.b
-    c_text.innerText = currentQuizData.c
-    d_text.innerText = currentQuizData.d
+
+var quesNo = document.getElementById("questionNo");
+var ansParent = document.getElementById("Answers");
+var questionShow = document.getElementById("dummyquestion");
+var score = document.getElementById("marks");
+var percentage = document.getElementById("percentage");
+var indexNum = 0;
+var marks = 0;
+function renderQuestion() {
+  var currentQue = questions[indexNum];
+  quesNo.innerHTML = "QUESTION:"+ (indexNum + 1) + "/" + questions.length;
+  questionShow.innerHTML = currentQue.ques;
+  ansParent.innerHTML = " ";
+
+  for (var i = 0; i < currentQue.opt.length; i++) {
+    var obj = {
+      question: currentQue.ques,
+      CorrectAns: currentQue.corect,
+      OptionSelected: currentQue.opt[i],
+    };
+    ansParent.innerHTML += `<div class="col-md-6 py-2 ">
+        <button onclick="checkQuestion('${currentQue.opt[i]}','${currentQue.corect}')">${currentQue.opt[i]}</button>
+    </div>`;
+  }
+  obj.id = Math.random().toString().slice(2);
+  let reference = ref(database, `Answers/Question${indexNum + 1}/${obj.id}/`);
+  set(reference, obj);
+  console.log(obj);
 }
-function deselectAnswers() {
-    answerEls.forEach(answerEl => answerEl.checked = false)
-}
-function getSelected() {
-    let answer
-    answerEls.forEach(answerEl => {
-        if(answerEl.checked) {
-            answer = answerEl.id
-        }
-    })
-    return answer
-}
-submitBtn.addEventListener('click', () => {
-    const answer = getSelected()
-    if(answer) {
-       if(answer === quizData[currentQuiz].correct) {
-           score++
-       }
-       currentQuiz++
-       if(currentQuiz < quizData.length) {
-           loadQuiz()
-       } else {
-           quiz.innerHTML = `
-           <p>You answered ${score}/${quizData.length} questions correctly</p>
-         
-           `
-       }
-    } })
+
+renderQuestion();
+window.checkQuestion = function (a, b) {
+  nextQuestion();
+
+  if (a == b) {
+    marks++;
+
+    score.innerHTML = marks;
+    percentage.innerHTML = (marks / 10) * 100 + "%";
+  }
+};
+
+window.nextQuestion = function () {
+  indexNum++;
+  if (indexNum == questions.length) {
+    var display1 = document.getElementById("main");
+    display1.style.display += " none";
+  }
+  renderQuestion();
+};
+
+
